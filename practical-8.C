c5652b12bf11b1d3c4077e6f83fd978f438090cf
JENIL.SAKHIYA-CE-07 <jenilsakhiya007@gmail.com>
2025-08-05 10:15:26 +0530

    Add files via upload

---
+++ practical-8.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+int age;
+printf("enter an age");
+scanf("%d",&age);
+(age>=18)? printf("you are aligible for open to account"): printf("you are not aligible for open to account");
+
+return 0;
+}
